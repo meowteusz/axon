00ec83a05243c50d4c6d6c72f98ccbf4b53d1d70
Mateusz Ryczek <mateusz@u.northwestern.edu>
2025-01-24 01:09:23 -0600

    refactor for abstraction, add dev mode

--- axon.js
+++ axon.js
@@ -19,6 +19,13 @@ const gippity = new OpenAIStream();
 function check_key() {
     let api_key = key_input.value;
 
+    if (api_key == "test") {
+        console.log("dev mode on");
+        key_input.style.setProperty("border-bottom-color", "#39FF77");
+        gippity.api_key = api_key;
+        return true;
+    }
+
     if (api_key.length == 0) {
         console.error("No API key provided.");
         key_input.style.setProperty("border-bottom-color", "#DADADA");
@@ -43,15 +50,39 @@ function check_key() {
     gippity.api_key = api_key;
 }
 
-async function mk_completion(query) {
+// Attached to the arrows on user messages ONLY
+async function respond(arrow) {
+    // Don't do anything unless the key is valid
+    if (gippity.api_key == "" || gippity.api_key == "CHANGE_ME") {
+        console.error("No API key provided.");
+        return false;
+    }
+
+    // Mark this arrow's path as taken
+    arrow.classList.add("active");
+
+    // Get the text from the user message
+    let query =
+        arrow.parentElement.parentElement.querySelector("textarea").value;
+
     const messages = [
         { role: "user", content: query },
     ];
+
+    // Append a blank system response message
+    let thread = arrow.parentElement.parentElement.parentElement;
+    thread.appendChild(system_message().cloneNode(true));
 
     // Should only ever be the one element with this class active
     let stream_here = document.querySelector(".stream_here");
 
     try {
+        if (gippity.api_key == "test") {
+            stream_here.value = "This is a test response.";
+            stream_here.classList.remove("stream_here");
+            return true;
+        }
+        
         const stream = await gippity.createCompletion(messages);
         let full_response = "";
 
@@ -68,32 +99,25 @@ async function mk_completion(query) {
     }
 }
 
-// Continue the main coversation thread
-async function continue_main() {
-    let textarea = document.querySelector(".context");
-    let context = textarea.value;
+// Attached to the arrows on system messages ONLY
+function new_message(arrow) {
+    // Mark this arrow's path as taken
+    arrow.classList.add("active");
 
-    if (context.length == 0) {
-        console.error("Write something");
-        return false;
-    }
+    // The parent of the parent of the arrow is
+    // the thread in question (be that a tangent
+    // or linear thread). This is an invariant.
+    let thread = arrow.parentElement.parentElement;
 
-    textarea.classList.add("sent");
-    document.querySelector(".current").classList.remove("current");
-    textarea.classList.remove("context");
-
-    chat_window.appendChild(system_message().cloneNode(true));
-
-    if (context != "test") {
-        await mk_completion(context);
-    }
-
-    return true;
+    // Button clicks only ever instantiate a new
+    // *user* message. This is another invariant.
+    thread.appendChild(user_message().cloneNode(true));
 }
 
 // Expose functions to the window object
 // lest ye suffer reference errors
-window.continue_main = continue_main;
+window.respond = respond;
+window.new_message = new_message;
 
 // Event Listeners
 key_input.addEventListener("input", check_key);
